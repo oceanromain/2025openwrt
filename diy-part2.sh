be71edf93c2116017a79f9311e20228154d8468b
18601121069 <startresnet@163.com>
2025-05-09 15:17:39 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -77,7 +77,7 @@ git clone https://github.com/kenzok8/golang  feeds/packages/lang/golang
 #sed -i '9d' ./feeds/oceanweb/luci-app-autotimeset/luasrc/controller/autotimeset.lua 
 sed -i 's/高级重启/关机/g' feeds/luci/applications/luci-app-advanced-reboot/po/zh-cn/advanced-reboot.po
 chmod +x feeds/kenzo/luci-app-lucky/luci-app-lucky/root/etc/init.d/lucky 
-chmod +x feeds/oceanweb/luci-app-autotimeset/root/etc/init.d/autotimeset
+#chmod +x feeds/oceanweb/luci-app-autotimeset/root/etc/init.d/autotimeset
 #chmod +x feeds/oceanweb/luci-app-beardropper/root/etc/init.d/beardropper
 #chmod +x feeds/oceanweb/luci-app-cowbping/root/etc/init.d/cowbping
 #sed -i 's#GO_PKG_TARGET_VARS.*# #g' feeds/packages/utils/v2dat/Makefile
